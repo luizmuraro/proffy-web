b94b497867a871d2a5e91c31cf5357ff6edc4709
Muraro <luizmuraro@gmail.com>
2020-08-05 20:59:13 -0300

    Add useState on TeachersList

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
@@ -9,13 +9,23 @@ import Select from '../../components/Select';
 import './styles.css';
 
 function TeacherList() {
+    const [subject, setSubject] = useState('');
+    const [week_day, setWeekDay] = useState('');
+    const [time, setTime] = useState('');
+
+    function searchTeachers(e: FormEvent) {
+        e.preventDefault();
+    }
+
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
-                <form id="search-teachers">
+                <form id="search-teachers" onSubmit={searchTeachers}>
                     <Select
                         name="subject"
                         label="Matéria"
+                        value={subject}
+                        onChange={e => { setSubject(e.target.value) }}
                         options={[
                             { value: 'Artes', label: 'Artes' },
                             { value: 'Biologia', label: 'Biologia' },
@@ -32,6 +42,8 @@ function TeacherList() {
                     <Select
                         name="week_day"
                         label="Dia da semana"
+                        value={week_day}
+                        onChange={e => { setWeekDay(e.target.value) }}
                         options={[
                             { value: '1', label: 'Segunda-feira' },
                             { value: '2', label: 'Terça-feira' },
@@ -42,7 +54,13 @@ function TeacherList() {
                             { value: '0', label: 'Domingo' },
                         ]}
                     />
-                    <Input name="time" label="Hora" />
+                    <Input
+                        type="time"
+                        name="time"
+                        label="Hora"
+                        value={time}
+                        onChange={(e) => { setTime(e.target.value) }}
+                    />
                 </form>
             </PageHeader>
 
